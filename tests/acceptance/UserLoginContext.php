d53ce355383de25833750ad68c21723afa6a0f5a
Luiz Felipe <lflimeira02@gmail.com>
2017-12-11 00:33:06 -0200

    Test: Add Sign in test
    Create files to test the Sign in failed scenario.

---
+++ tests/acceptance/UserLoginContext.php
@@ -0,0 +1,62 @@
+<?php
+
+use Behat\MinkExtension\Context\RawMinkContext;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+class UserLoginContext extends RawMinkContext
+{
+
+    /**
+     * @Given I'm on Login page
+     */
+    public function imOnLoginPage()
+    {
+        $this->getSession()->visit($this->locatePath('/'));
+    }
+
+    /**
+     * @When I fill email with :email
+     */
+    public function iFillEmailWith($email)
+    {
+        $this->getSession()->wait(
+            5000, 
+            "document.querySelector('#email').style.display != 'none'"
+        );
+        $this->getSession()->getPage()->fillField("email", $email);
+    }
+
+    /**
+     * @When fill password with :password
+     */
+    public function fillPasswordWith($password)
+    {
+        $this->getSession()->getPage()->fillField("password", $password);
+    }
+
+    /**
+     * @When press button Sign in
+     */
+    public function pressButtonSignIn()
+    {
+        $this->getSession()->getPage()->pressButton("signin");
+    }
+
+    /**
+     * @Then an error message should be shown
+     */
+    public function anErrorMessageShouldBeShown()
+    {
+        $this->getSession()->wait(3000);
+        $page = $this->getSession()->getPage();
+
+        $failMessage = $page->find('css', 'h1')->getText();
+
+        \PHPUnit_Framework_TestCase::assertEquals(
+            'Failed to login.',
+            $failMessage
+        );
+    }
+
+}
---
+++ tests/acceptance/features/user_login.feature
@@ -0,0 +1,11 @@
+Feature: Sign in User
+    As a user of website
+    I want to sign in
+    So that I can access my user private page
+
+    Scenario: Fail to sign in
+        Given I'm on Login page
+        When I fill email with "xxy@gmail.com"
+        And fill password with "123" 
+        And press button Sign in
+        Then an error message should be shown
